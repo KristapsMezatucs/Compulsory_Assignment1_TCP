bf0f3d43622906f5db6d28d5927a02f0381768c0
Sherifsss <kristaps_mezatucs2@inbox.lv>
2021-10-13 22:17:01 +0200

    Add project files.

---
+++ Compulsory_Assignment1_TCP/BookRepo.cs
@@ -0,0 +1,37 @@
+﻿using Compulsory_Assignment1_Model_Book;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compulsory_Assignment1_TCP
+{
+    public class BookRepo
+    {
+        public static readonly List<Book> bookList = new List<Book>()
+        {
+            new Book() { Author="J.K.Rowling", Title="Harry Potter", PageNr = 300, ISBN13 = "12345767862"},
+            new Book() { Author="Andrew Wiggins", Title="Race of a century", PageNr = 200, ISBN13 = "12345767842"},
+            new Book() { Author="Pedro Rainho", Title="C# for dummies", PageNr = 400, ISBN13 = "12309767862"}
+        };
+
+        public List<Book> GetAll()
+        {
+            List<Book> result = new List<Book>(bookList);
+            return result;
+        }
+
+        public Book Get(string isbn13)
+        {
+            return bookList.Find(book => book.ISBN13 == isbn13);
+        }
+
+        public Book Add(Book b)
+        {
+            bookList.Add(b);
+            return b;
+        }
+
+    }
+}
---
+++ Compulsory_Assignment1_TCP/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Compulsory_Assignment1_Model_Book;
+
+namespace Compulsory_Assignment1_TCP
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            TcpListener listener = new TcpListener(System.Net.IPAddress.Loopback, 4646);
+            listener.Start();
+            Console.WriteLine("Server started");
+
+
+            while (true)
+            {
+                TcpClient socket = listener.AcceptTcpClient();
+                Task.Run(() =>
+                {
+                    HandleClient(socket);
+                });
+            }
+        }
+
+
+        public static void HandleClient(TcpClient socket)
+        {
+
+            NetworkStream ns = socket.GetStream();
+            StreamReader reader = new StreamReader(ns);
+            StreamWriter writer = new StreamWriter(ns);
+            BookRepo repo = new BookRepo();
+            Console.WriteLine("Client connected");
+
+            string message = reader.ReadLine();
+            Console.WriteLine("Client wrote " + message);
+            writer.WriteLine(message);
+            writer.Flush();
+            socket.Close();
+
+
+            while (true)
+            {
+                string command = reader.ReadLine();
+                string parameter = reader.ReadLine();
+
+                if (command == "GetAll" && string.IsNullOrEmpty(parameter))
+                {
+                    List<Book> books = repo.GetAll();
+
+                    var options = new JsonSerializerOptions { WriteIndented = true };
+                    string jsonstring = JsonSerializer.Serialize(books, options);
+
+                    writer.WriteLine(jsonstring);
+                    writer.Flush();
+                }
+                else if (command == "Get" && BookRepo.bookList.Any(b => b.ISBN13 == parameter))
+                {
+                    Book fb = repo.Get(parameter);
+
+                    var options = new JsonSerializerOptions { WriteIndented = true };
+                    string jsonstring = JsonSerializer.Serialize(fb, options);
+
+                    writer.WriteLine(jsonstring);
+                    writer.Flush();
+                }
+                else if (command == "Save" && !string.IsNullOrEmpty(parameter))
+                {
+                    try
+                    {
+                        Book b = JsonSerializer.Deserialize<Book>(parameter);
+                        repo.Add(b);
+
+                        writer.WriteLine("Book was added");
+                        writer.Flush();
+
+                    }
+
+                    catch(Exception e)
+                    {
+                        writer.WriteLine(e.Message);
+                        writer.WriteLine("Invalid json");
+                        writer.Flush();
+                    }
+                   
+                }
+
+                else
+                {
+                    writer.WriteLine("Invalid command, current commands are GetAll, Get, Save");
+                    writer.Flush();
+                }
+
+            }
+
+        }
+
+    }
+}
